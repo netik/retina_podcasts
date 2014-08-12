6644dfc26320c14415af2f2e64eac4809cf7c95e
John Adams <jna@retina.net>
2014-08-12 16:29:02 +0000

    support multiple shows

--- html/tools/renderpage.py
+++ html/tools/renderpage.py
@@ -5,7 +5,7 @@
 #
 #   * State of icecast server (if we're live) 
 #   * All available podcasts rendered from
-#     showindex.json and mustache templates.
+#     showindex_multi.json and mustache templates.
 #
 # J. Adams < jna@retina.net >
 # 8/5/2014
@@ -23,6 +23,7 @@ import time
 class Index(object):
   """ methods for the index.mustache page """
   def is_live(self):
+      """ right now we only broadcast under /wg, see if it is up """
       r = requests.get('http://podcast.retina.net:8000/')
       if r.status_code == 200:
           if r.text.find("Mount Point /wg") > -1:
@@ -31,16 +32,18 @@ class Index(object):
           return False
 
   def shows(self):
-    fp = open("showindex.json","r")
+    fp = open("showindex_multi.json","r")
     shows = json.load(fp)
     return shows['shows']
 
 class Rss(object):
   """ methods for rendering the rss index """
   def shows(self):
-    fp = open("showindex.json","r")
+    fp = open("showindex_multi.json","r")
     shows = json.load(fp)
-    return shows['shows']
+
+    # this RSS feed is only the first show (wg) episode list. 
+    return shows['shows'][0]['show']['episodes']
 
 # set up the renderer
 renderer = pystache.Renderer(search_dirs="/retina/podcast/html/templates")
